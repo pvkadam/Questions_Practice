dfcf2bfe4c167fcaae82fe8c3eb965f3581d733b
Ravi Chaganti <ravikrishna@Ravis-MacBook-Air.local>
2016-06-13 08:31:39 -0400

    ATOI function added

---
+++ Strings/src/ATOI.java
@@ -0,0 +1,49 @@
+public class ATOI {
+	public int atoi(final String a) {
+	    
+	    if (a.length() == 0) {
+	        return 0;
+	    }
+	    
+	    String temp = a.trim();
+	    
+	    char flag = '+';
+	    int i = 0;
+	    
+	    if (temp.charAt(0) == '-') {
+	        flag = '-';
+	        i++;
+	    }
+	    
+	    else if (temp.charAt(0) == '+') {
+	        flag = '+';
+	        i++;
+	    }	  
+	    
+	    else if (temp.charAt(0) == ' ') {
+	        i++;
+	    }
+	    
+	    double result = 0;
+	    
+	    while (temp.length() > i && temp.charAt(i) >= '0' && temp.charAt(i) <= '9') {
+	        result = result*10 + (temp.charAt(i) - '0');
+	        i++;
+	    }
+	    
+	    if (flag == '-') {
+	        result = -result;
+	    }
+	    
+	    if (result > Integer.MAX_VALUE) {
+	        result = Integer.MAX_VALUE;
+	    }
+	    
+	    else if (result < Integer.MIN_VALUE) {
+	        result = Integer.MIN_VALUE;
+	    }
+	    
+	    return (int) result;
+	    
+	}
+}
