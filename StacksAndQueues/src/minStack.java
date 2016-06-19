83b44b4119a672ed8e7cb064e7335dce6ec903fa
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-06-19 00:06:17 -0400

    min-stack

---
+++ StacksAndQueues/src/minStack.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class minStack {
+	
+	private Stack<Integer> stack = new Stack<Integer>();
+	
+	private int min = -1;
+		
+	
+	//keep adding elements to the stack
+	//compare it with minimum element (-1) and update it every time
+	public void push(int x) {
+		if (stack.isEmpty() || x <= min) {
+			stack.push(min);
+			min = x;
+		}
+        stack.push(x);
+    }
+
+	//only remove top if min is at top 
+    public void pop() {
+        if (!stack.isEmpty() && min == stack.pop()) {
+        	min = stack.pop();
+        }
+    }
+
+    //simple top 
+    public int top() {
+        if (stack.isEmpty()) {
+        	return -1;
+        }
+        return stack.peek();
+    }
+
+    	
+    //simply return min
+    public int getMin() {
+        return min;
+    }
+}
---
+++ StacksAndQueues/src/minStackElement.java
@@ -0,0 +1,13 @@
+
+public class minStackElement {
+	public int value;
+	public int min;
+	public minStackElement next;
+	
+	public minStackElement(int value, int min) {
+		this.value = value;
+		this.min = min;
+
+	}
+
+}
