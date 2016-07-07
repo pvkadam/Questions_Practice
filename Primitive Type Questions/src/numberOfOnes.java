220a378733811ddac645746eae9d266e2596c677
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-07-07 11:58:38 -0400

    number of ones

---
+++ Primitive Type Questions/src/numberOfOnes.java
@@ -0,0 +1,26 @@
+/*
+ * Write a function that takes an unsigned integer and returns the number of 1 bits it has.
+
+Example:
+
+The 32-bit integer 11 has binary representation
+
+00000000000000000000000000001011
+so the function should return 3.
+
+
+ */
+public class numberOfOnes {
+	public int numSetBits(long a) {
+	    int zeroes = 0;
+	    
+	    while (a!=0) {
+	        if ((a & 1) == 1) {
+	            zeroes++;
+	        }
+	        a >>= 1;
+	    }
+	    
+	    return zeroes;
+	}
+}
