fcbe48b37f17b74b239f2bbae0983e5ca80bd1df
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-07-09 03:57:06 -0400

    shortest unique prefix - added example of trie - updated

--- BinarySearchTree/src/binarySearchTree/shortestUniquePrefix.java
+++ BinarySearchTree/src/binarySearchTree/shortestUniquePrefix.java
@@ -36,6 +36,16 @@ dove = dov
                                                  \
                                                  Leaf
                                                  (zebra)
+                                                 
+                                                 
+ 1) Construct a Trie of all words. Also maintain frequency of every node (Here frequency is 
+   number of times node is visited during insertion). Time complexity of this step is O(N) 
+   where N is total number of characters in all words.
+
+2) Now, for every word, we find the character nearest to the root with frequency as 1. 
+The prefix of the word is path from root to this character. To do this, 
+we can traverse Trie starting from root. For every node being traversed, we check its frequency. 
+If frequency is one, we print all characters from root to this node and don’t traverse down this node.
 
  */
 
