864009261e46d3c085225688c7fae69fb8da1ecc
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-07-16 03:20:42 -0400

    added another method to reverse bits

--- Primitive Type Questions/reverseBits.java
+++ Primitive Type Questions/reverseBits.java
@@ -21,6 +21,18 @@ public class reverseBits {
 		System.out.println(b);
 	}
 	
+	//another method
+    public long reverse(long a) {
+        long result = 0;
+        int i = 31;
+        while(a > 0){
+            result += (a % 2) * Math.pow(2, i);
+            i--;                        
+            a = a/2;
+        }
+        return result;
+    }
+	
 	public static void main (String[] args) {
 		reverseBitsMethod(6); 
 	}
