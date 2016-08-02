d55d6559327317f4ca43bd34582aec8808c7a007
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-08-02 12:46:44 -0400

    deleted some redundant files

--- Math/src/SeiveAlgorithm.java
+++
@@ -1,6 +0,0 @@
-import java.util.*;
-
-//used to check prime numbers up to a certain number
-public class SeiveAlgorithm {
-
-}
--- Math/src/gridUniquePaths.java
+++ Math/src/gridUniquePaths.java
@@ -18,8 +18,8 @@ Output : 2
               OR  : (0, 0) -> (1, 0) -> (1, 1)
  */
 public class gridUniquePaths {
-	public int uniquePaths(int a, int b) {
-	    
+	public static int uniquePaths(int a, int b) {
+	     
 	    int[][] array = new int[a][b];
 	    
 	    for (int i = 0; i < a; i++) {
@@ -35,7 +35,11 @@ public class gridUniquePaths {
 	            array[i][j] = array[i-1][j] + array[i][j-1];
 	        }
 	    }
-	    
+	    System.out.println(array[a-1][b-1]);
 	    return array[a-1][b-1];
+	}
+	
+	public static void main (String[] args) {
+		uniquePaths(4,5);
 	}
 }
