7ac2fe02a4ca9b955e3aed1fe128726576f51926
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-08-03 06:47:22 -0400

    updated kth row of pascal triangle - smart solution

--- Arrays/src/kthRowPascalTriangle.java
+++ Arrays/src/kthRowPascalTriangle.java
@@ -5,19 +5,16 @@
 import java.util.*;
 
 public class kthRowPascalTriangle {
-	public List<Integer> getRow(int rowIndex) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-	 
-		if (rowIndex < 0)
-			return result;
-	 
-		result.add(1);
-		for (int i = 1; i <= rowIndex; i++) {
-			for (int j = result.size() - 2; j >= 0; j--) {
-				result.set(j + 1, result.get(j) + result.get(j + 1));
-			}
-			result.add(1);
-		}
-		return result;
-	}
+    public ArrayList<Integer> getRow(int rowIndex) {
+    	
+    	ArrayList<Integer> result = new ArrayList<Integer>();
+    	
+    	result.add(1);
+    	
+    	for (int i = 0; i < rowIndex; i++) {
+    	    result.add(result.get(i)*(rowIndex-i)/(i+1));
+    	}
+    	
+    	return result;
+    }
 }
