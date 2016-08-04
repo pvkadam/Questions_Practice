06abc283a22d937242c87fb10a19295604a1a764
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-08-04 07:02:17 -0400

    updated reverse words in string

--- Strings/src/reverseStringWords.java
+++ Strings/src/reverseStringWords.java
@@ -9,22 +9,21 @@ import java.util.*;
 
 public class reverseStringWords {
 	public String reverseWords(String a) {
-		    
-		if (a == null || a.length() == 0) {
+		
+        if (a == null || a.length() == 0) {
 			return "";
 		}
  
 		// split to words by space
 		String[] arr = a.split(" ");
 		StringBuilder sb = new StringBuilder();
-		for (int i = arr.length - 1; i >= 0; --i) {
+		for (int i = arr.length - 1; i >= 0; i--) {
 			if (!arr[i].equals("")) {
 				sb.append(arr[i]).append(" ");
 			}
 		}
 		
-		System.out.println(sb);
-		return sb.length() == 0 ? "" : sb.substring(0, sb.length() - 1);
+		return sb.substring(0, sb.length() - 1);
 	}
 	
 	public static void main (String[] args) {
