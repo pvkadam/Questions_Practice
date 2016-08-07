ebbbf3fc21210d67d0216b531d8e3635a36934fe
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-08-07 08:38:38 -0400

    Cyclic list optimized solution

--- LinkedLists/src/CyclicList.java
+++ LinkedLists/src/CyclicList.java
@@ -1,31 +1,22 @@
+import java.util.*;
 
 public class CyclicList {
 	
 	//Also known as Floyd's cycle finding algorithm
-	   public ListNode detectCycle(ListNode a) {
-	        ListNode fast = a;
-	        ListNode slow = a;
-	        
-	        boolean hasCycle = false;
-	        while(fast != null && fast.next != null && slow != null){
-	            fast = fast.next.next;
-	            slow = slow.next;
-	            if(fast == slow){
-	                hasCycle = true;
-	                break;
-	            }
+	public ListNode detectCycle(ListNode A) {
+
+	    HashSet<ListNode> hashSet = new HashSet<ListNode>();
+	    
+	    while (A != null) {
+	        if (hashSet.contains(A)) {
+	            return A;
+	        } else {
+	            hashSet.add(A);
 	        }
-	        
-	        if(!hasCycle) return null;
-	        
-	        else{
-	            fast = a;
-	            while(fast != slow){
-	                fast = fast.next;
-	                slow = slow.next;
-	            }
-	            return fast;
-	        }
-	   }
+	        A = A.next;
+	    }
+	    
+	    return null;
+	}
 	   
 }
