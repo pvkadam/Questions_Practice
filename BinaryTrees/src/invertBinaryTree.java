2460eb9c0aefca7e23ac28344ddef13b948c2f8b
Ravi Chaganti <ravi.chaganti@mail.mcgill.ca>
2016-08-13 06:41:09 -0400

    updated invert binary tree

--- BinaryTrees/src/invertBinaryTree.java
+++ BinaryTrees/src/invertBinaryTree.java
@@ -1,29 +1,41 @@
+/*
+ * Given binary tree
 
+     1
+   /   \
+  2     3
+ / \   / \
+4   5 6   7
+invert and return
+
+     1
+   /   \
+  3     2
+ / \   / \
+7   6 5   4
+
+ */
 public class invertBinaryTree {
-    public TreeNode invertTree(TreeNode root) {
-    	
-    	if (root == null) {
-    		return null;
-    	}
-    	
-    	helper(root);
-    	
-    	return root;
-    	
-    }
-    
-    public void helper (TreeNode node) {
-    	TreeNode temp = node.left;
-    	node.left = node.right;
-    	node.right = temp;
-    	
-    	if (node.left != null) {
-    		helper(node.left);
-    	}
-    	
-    	if (node.right != null) {
-    		helper(node.right);
-    	}
-    }
+	   public TreeNode invertTree(TreeNode root) {
+	        
+	        TreeNode current = root; 
+	        
+	        if (current == null) {
+	            return null;
+	        }
+	        
+	        //swap values
+	        TreeNode temp = current.left;
+	        current.left = current.right;
+	        current.right = temp;
+	        
+	        //go to next nodes
+	        invertTree(current.left);
+	        invertTree(current.right);
+	        
+	    	
+	    	return root;
+	    	
+	    }
     
 }
